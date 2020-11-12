7a251a2dd286cd286a8dde23a6dfc0b98013c910
pongdanny <pongdaniel17@gmail.com>
2020-11-12 18:21:20 -0500

    project complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import HeadingA from "./css-modules/HeadingA";
 import HeadingB from "./css-modules/HeadingB";
 import Image from "./image/Image";
 import ClassComponent from "./class-component/ClassComponent";
 
 function App() {
-	return (
-		<div>
-			<h1>Exploring React Builds</h1>
-			<HeadingA />
-			<HeadingB />
-			<Image />
-			<ClassComponent />
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>{process.env.REACT_APP_TITLE}</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/image">Image</Link>
+            </li>
+            <li>
+              <Link to="/class-component">Class Component</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/image">
+            <Image />
+          </Route>
+          <Route path="/class-component">
+            <ClassComponent />
+          </Route>
+          <Route path="/">
+            <HeadingA />
+            <HeadingB />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
